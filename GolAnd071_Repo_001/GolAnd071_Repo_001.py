2ab53bda06d8d92969fcb5e16dcfd2b0eb7d294a
GolAnd071 <goland071@gmail.com>
2021-10-01 00:29:54 +0300

    Added homework (I've done it not exactly, but I've written all functions, so in my opinion main idea was saved. These functions colud be used for doing classwork as well).

--- GolAnd071_Repo_001/GolAnd071_Repo_001.py
+++ GolAnd071_Repo_001/GolAnd071_Repo_001.py
@@ -90,6 +90,96 @@ def ex4():
     circle(screen, (0, 0, 0), (285, 440), 5)
 
 
+def drawBckgrnd(scr):
+    rect(scr, (125, 125, 125), (0, 0, 400, 250))
+
+
+def drawHs(scr, stPnt, scl=1):
+    rect(scr, (54, 44, 18), (stPnt[0], stPnt[1], 200 * scl, 300 * scl))
+    rect(
+        scr,
+        (53, 23, 12),
+        (stPnt[0] + 30 * scl, stPnt[1] + 220 * scl, 40 * scl, 50 * scl),
+    )
+    rect(
+        scr,
+        (53, 23, 12),
+        (stPnt[0] + 80 * scl, stPnt[1] + 220 * scl, 40 * scl, 50 * scl),
+    )
+    rect(
+        scr,
+        (255, 255, 0),
+        (stPnt[0] + 130 * scl, stPnt[1] + 220 * scl, 40 * scl, 50 * scl),
+    )
+    rect(
+        scr,
+        (50, 50, 50),
+        (stPnt[0] - 20 * scl, stPnt[1] + 150 * scl, 240 * scl, 30 * scl),
+    )
+    for i in range(4):
+        rect(
+            scr,
+            (172, 138, 100),
+            (stPnt[0] + (20 + i * 130 / 3) * scl, stPnt[1], 30 * scl, 150 * scl),
+        )
+    for i in range(7):
+        rect(
+            scr,
+            (50, 50, 50),
+            (stPnt[0] - (10 - i * 35) * scl, stPnt[1] + 120 * scl, 10 * scl, 30 * scl),
+        )
+    rect(scr, (50, 50, 50), (stPnt[0], stPnt[1] + 110 * scl, 200 * scl, 10 * scl))
+    polygon(
+        scr,
+        (0, 0, 0),
+        (
+            (stPnt[0] - 20 * scl, stPnt[1]),
+            (stPnt[0] + 20 * scl, stPnt[1] - 30 * scl),
+            (stPnt[0] + 180 * scl, stPnt[1] - 30 * scl),
+            (stPnt[0] + 220 * scl, stPnt[1]),
+        ),
+    )
+
+
+def drawGhst(scr, stPnt, scl=1, hScl=1):
+    polygon(
+        scr,
+        (200, 200, 200),
+        (
+            (stPnt[0], stPnt[1]),
+            (stPnt[0] + 20 * scl * hScl, stPnt[1] - 100 * scl),
+            (stPnt[0] + 40 * scl * hScl, stPnt[1] - 110 * scl),
+            (stPnt[0] + 100 * scl * hScl, stPnt[1] - 25 * scl),
+            (stPnt[0] + 90 * scl * hScl, stPnt[1] - 5 * scl),
+            (stPnt[0] + 60 * scl * hScl, stPnt[1] - 25 * scl),
+            (stPnt[0] + 50 * scl * hScl, stPnt[1] - 15 * scl),
+            (stPnt[0] + 20 * scl * hScl, stPnt[1] - 25 * scl),
+        ),
+    )
+    circle(
+        scr,
+        (200, 200, 200),
+        (stPnt[0] + 30 * scl * hScl, stPnt[1] - 105 * scl),
+        20 * scl,
+    )
+    circle(scr, (0, 0, 0), (stPnt[0] + 20 * scl * hScl, stPnt[1] - 110 * scl), 5 * scl)
+    circle(scr, (0, 0, 0), (stPnt[0] + 35 * scl * hScl, stPnt[1] - 110 * scl), 5 * scl)
+
+
+def ex5():
+    screen = pygame.display.set_mode((400, 600))
+    drawBckgrnd(screen)
+    drawHs(screen, (20, 220), 0.5)
+    drawHs(screen, (150, 170), 0.5)
+    drawHs(screen, (290, 120), 0.5)
+    circle(screen, (255, 255, 255), (360, 40), 30)
+    ellipse(screen, (100, 100, 100), (180, 20, 200, 30))
+    ellipse(screen, (70, 70, 70), (50, 30, 200, 30))
+    drawGhst(screen, (250, 550), 1, -1)
+    drawGhst(screen, (250, 550))
+    drawGhst(screen, (100, 550), 0.5)
+
+
 if __name__ == "__main__":
     eval("ex" + input() + "()")
 
